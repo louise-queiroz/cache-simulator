617fd2aa5b6be745a8ee91ddee3cf7ea5f924eac
louise-queiroz <louisequeiroz8@gmail.com>
2023-04-09 00:40:45 -0300

    totalmente associativo implementado

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ E após instalação, instalar a biblioteca:
 pip install numpy
 
 python3 main.py 256 4 1 R 1 bin_100.bin
-python3 main.py 16 2 8 R 1 bin_10000.bin
+python3 main.py 1 4 32 R 1 vortex.in.sem.persons.bin
 
 gcc arquivo.c -o arquivo
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, math, struct
+import sys, math, struct, random
 import numpy as np
 
 def main():
@@ -31,15 +31,18 @@ def main():
 	miss_capacidade = 0
 	miss_conflito = 0
 	hit = 0
+	misses = 0
 	missRate = 0
 	hitRate = 0
-	miss2Rate = 0
+	endereco = 0
+	cont = 0
+	disp = 0
 
-
+	#print(matriz)
 #calcula o número de bits offset, indice e tag
 	n_bits_offset = math.log2(bsize)
 	n_bits_indice = math.log2(nsets)
-
+	
 	n_bits_tag = 32 - n_bits_offset - n_bits_indice
 
 #abrir arquivo
@@ -60,7 +63,7 @@ def main():
 			#indice
 			indice = addr >> int(n_bits_offset) & int((math.pow(2,n_bits_indice)-1))
 			mod = indice % nsets
-			
+
 			#função mapeamento direto
 			if assoc == 1 :
 				if cache_val[mod] == 0:
@@ -74,10 +77,46 @@ def main():
 						miss_conflito +=1
 						cache_val[mod] = 1
 						cache_tag[mod] = tag
+			#função totalmente associativa
+			elif nsets == 1:
+				cont = 0
+				disp = 0
+				#procura o valor em todos os blocos
+				for i in range(assoc):
+					if cache_tag[i] == tag and cache_val[i] == 1:
+						hit +=1
+						cont = 1
+						break
+				#tratamento da falta
+				if cont == 0:
+					while disp == 0:
+						for i in range(assoc):
+							if cache_val[i] == 0:
+								cache_val[i] = 1
+								miss_compulsorio +=1
+								cache_tag[i] = tag
+								disp = 1
+								break
+						disp = 2
+					if disp == 2:
+						miss_capacidade +=1
+						endereco = subRandom(0, (assoc-1))
+						cache_tag[endereco] = tag
+			else: #futura assoc conjunto
+				erro += 1
+				print("erro")
 
+		misses = miss_conflito + miss_capacidade + miss_compulsorio
 		missRate = (miss_conflito + miss_capacidade + miss_compulsorio)/acessos
+		miss_compulsorio = miss_compulsorio / misses
+		miss_capacidade = miss_capacidade/misses
+		miss_conflito = miss_conflito/misses
 		hitRate = hit / acessos
-		print(acessos, hitRate, missRate, miss_compulsorio, miss_capacidade, miss_conflito)
+
+		print(acessos,"{:.4f} {:.4f} {:.4f} {:.4f} {:.4f}". format(hitRate, missRate, miss_compulsorio, miss_capacidade, miss_conflito))
+
+def subRandom(valora, valorb):
+	return random.randint(valora, valorb)
 
 if __name__ == '__main__':
 	main() 
